09f12dd1f142c7aebeb88d5970384fa6551f219c
Ajay kumar <ajaykwins@gmail.com>
2021-09-08 11:37:27 +0530

    theme color change

--- src/app/feature/customer-onboarding/customer-modal/customer-modal.component.ts
+++ src/app/feature/customer-onboarding/customer-modal/customer-modal.component.ts
@@ -21,8 +21,8 @@ export interface PeriodicElement {
   schedule: string;
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  { time: '9:00 AM', schedule: '' },
-  { time: '1:00 PM', schedule: '' },
-  { time: '3:00 PM', schedule: '' },
-  { time: '6:00 PM', schedule: '' },
+  { time: '9:00 AM', schedule: '9:00 AM - 12:00 PM' },
+  { time: '1:00 PM', schedule: '1:00 PM - 2:00 PM' },
+  { time: '3:00 PM', schedule: '3:00 PM - 5:00 PM' },
+  { time: '6:00 PM', schedule: '6:00 PM - 9:00 PM' },
 ];
