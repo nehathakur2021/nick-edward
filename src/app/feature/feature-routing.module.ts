167d2574b9bd46cfada0813065bf333114d2dce6
Ajay kumar <ajaykwins@gmail.com>
2021-09-09 11:50:41 +0530

    UI CHANGES

--- src/app/feature/feature-routing.module.ts
+++ src/app/feature/feature-routing.module.ts
@@ -9,70 +9,64 @@ import { UsersComponent } from './users/users.component';
 import { TechnicianComponent } from './technician/technician.component';
 import { TechnicianListComponent } from './technician-list/technician-list.component';
 import { AppointmentDetailComponent } from './appointment-detail/appointment-detail.component';
-<<<<<<< Updated upstream
 import { ProfileComponent } from './profile/profile.component';
 import { ScheduleComponent } from './schedule/schedule.component';
-=======
 import { ManageCategoryComponent } from './manage-category/manage-category.component';
 import { NotificationComponent } from './notification/notification.component';
->>>>>>> Stashed changes
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children:[
+    children: [
       {
-        path:'dashboard',
-        component:DashboardComponent
-     },
-     {
-      path:'customer-type',
-      component:CustomerTypeComponent
-     },
-     {
-      path:'customer-onboarding',
-      component:CustomerOnboardingComponent
-     },
-     {
-      path:'users',
-      component:UsersComponent
-     },
-     {
-      path:'technician',
-      component:TechnicianComponent
-     },
-     {
-      path:'technician-list',
-      component:TechnicianListComponent
-     },
-     {
-      path:'appoinment-detail',
-      component:AppointmentDetailComponent
-     },
-     {
-      path:'manage-category',
-      component:ManageCategoryComponent
-     },
-     {
-      path:'jobs',
-      component:JobsComponent
-     },
-     {
-<<<<<<< Updated upstream
-      path:'profile',
-      component:ProfileComponent
-     },
-     {
-      path:'schedule',
-      component:ScheduleComponent
-     },
-
-=======
-      path:'notification',
-      component:NotificationComponent
-     }
->>>>>>> Stashed changes
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'customer-type',
+        component: CustomerTypeComponent
+      },
+      {
+        path: 'customer-onboarding',
+        component: CustomerOnboardingComponent
+      },
+      {
+        path: 'users',
+        component: UsersComponent
+      },
+      {
+        path: 'technician',
+        component: TechnicianComponent
+      },
+      {
+        path: 'technician-list',
+        component: TechnicianListComponent
+      },
+      {
+        path: 'appoinment-detail',
+        component: AppointmentDetailComponent
+      },
+      {
+        path: 'manage-category',
+        component: ManageCategoryComponent
+      },
+      {
+        path: 'jobs',
+        component: JobsComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+      },
+      {
+        path: 'schedule',
+        component: ScheduleComponent
+      },
+      {
+        path: 'notification',
+        component: NotificationComponent
+      }
     ]
 
   }
